33c6eb1fc95d7a0d5c50e6d6b63d95d58c1fb178
Marcin TORGiren Fabrykowski <marcin@fabrykowski.pl>
2014-06-21 03:27:22 +0200

    wywalenie niepotrzebnego liczenia

--- index.php
+++ index.php
@@ -2,15 +2,5 @@
 echo "A ja jestem phpcem<br/>";
 echo "2+2=".(2+2)."<br/>";
 echo $_SERVER["SERVER_ADDR"];
-$i=0;
-while($i < 10000)
-{
-	$i++;
-	$j = $i * $i;
-	for($l=0; $l<$i; $l++)
-	{
-		$o=100*100*100;
-	}
-}
 ?>
 
